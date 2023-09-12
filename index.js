9ebede2aa8c5fbaa0e0861f9596c08ad41941753
+ <+>
2023-09-12 17:49:19 -0400

    Done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2;
 
+const random = Math.floor(Math.random() * 13)
+
+const mod = 4
+const num4 = 4
+const num3 = 4
+
+const max = 20
 // Write your code here
